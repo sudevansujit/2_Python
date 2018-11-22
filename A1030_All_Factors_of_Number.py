54db9711d32633da42e906266db4c34f6b74dc4e
Sujit Sudevan <44827942+sudevansujit@users.noreply.github.com>
2018-11-22 19:01:46 +0530

    Add files via upload

---
+++ A1030_All_Factors_of_Number.py
@@ -0,0 +1,12 @@
+# Find all Factors of a Number
+
+def num_factors(x):
+
+    print("The factors of " ,x , "are:")
+
+    for i in range(1,(x+1)):
+        if x %i == 0 :
+            print(i)
+
+num = int(input("Enter a number:   "))
+num_factors(num)

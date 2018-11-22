b534335815e3617dbfc7758c64ae618997d78e54
Sujit Sudevan <44827942+sudevansujit@users.noreply.github.com>
2018-11-22 20:05:04 +0530

    * *

--- A1040_Display_Calendar.py
+++ A1040_Display_Calendar.py
@@ -8,3 +8,52 @@ mm = int(input("Enter month: "))
 
 
 print(calendar.month(yy, mm))         # display the calendar  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#     O/P
+#     Enter year: 2018
+#     Enter month: 11
+#     November 2018
+#     Mo Tu We Th Fr Sa Su
+#               1  2  3  4
+#      5  6  7  8  9 10 11
+#     12 13 14 15 16 17 18
+#     19 20 21 22 23 24 25
+#     26 27 28 29 30

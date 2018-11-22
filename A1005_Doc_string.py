f21ef88862b6d8f312f73a091a21a0293e72704a
Sujit Sudevan <44827942+sudevansujit@users.noreply.github.com>
2018-11-22 17:05:40 +0530

    Add files via upload

---
+++ A1005_Doc_string.py
@@ -0,0 +1,13 @@
+#"""It is a string that occurs as the first statement in a module,
+#function, class, or method definition. We must write what a
+#function/class does in the docstring."""
+
+
+def double(num):
+    """Function to double the value"""
+    return 2*num
+
+num=2
+
+print(double.__doc__)
+print(double(num))

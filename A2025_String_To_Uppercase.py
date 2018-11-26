aede00d35cb8f4e393bcc61d2317c47b57c3e97e
Sujit Sudevan <44827942+sudevansujit@users.noreply.github.com>
2018-11-26 20:08:52 +0530

    * *

--- A2025_String_To_Uppercase.py
+++ A2025_String_To_Uppercase.py
@@ -6,3 +6,47 @@ string1 = string.upper()
  
 print("\nOriginal String in Lowercase  =  ", string)
 print("The Given String in Uppercase =  ", string1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#    O/P
+#    Original String in Lowercase  =   Python
+#    The Given String in Uppercase =   PYTHON

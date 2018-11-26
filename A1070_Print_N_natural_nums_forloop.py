8fc45ec2c14ac6544d942adb92d2f74f6cf958fe
Sujit Sudevan <44827942+sudevansujit@users.noreply.github.com>
2018-11-26 12:10:38 +0530

    * *

--- A1070_Print_N_natural_nums_forloop.py
+++ A1070_Print_N_natural_nums_forloop.py
@@ -8,3 +8,55 @@ for i in range(1, number + 1):
 
     print (i)
     
+
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+#  Please Enter any Number: 10
+#  The List of Natural Numbers from 1 to 10 are
+#  1
+#  2
+#  3
+#  4
+#  5
+#  6
+#  7
+#  8
+#  9
+#  10    
+ 

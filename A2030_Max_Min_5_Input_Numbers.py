75a8b5777f900c7afd275563e99634912d9abb00
Sujit Sudevan <44827942+sudevansujit@users.noreply.github.com>
2018-11-26 20:20:40 +0530

    * * *

---
+++ A2030_Max_Min_5_Input_Numbers.py
@@ -0,0 +1,17 @@
+# Max and Min of 5 Input Numbers
+
+print("Enter Five Numbers between 1 and 100")
+a,b,c,d,e = int(input("a=  ")),int(input("b=  ")),int(input("c=  ")),int(input("d=  ")),int(input("e=  "))
+list = [a,b,c,d,e]
+
+count = 0
+for item in list:
+    if(item > count):
+        count = item
+print(count, "is Maximum")
+
+number = 101
+for i in list:
+    if i < number:
+        number = i
+print(number, "is Minimum")

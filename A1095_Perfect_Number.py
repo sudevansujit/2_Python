6eb26a0c93872e118cf090d461f04653af351c68
Sujit Sudevan <44827942+sudevansujit@users.noreply.github.com>
2018-11-26 16:24:40 +0530

    * * *

---
+++ A1095_Perfect_Number.py
@@ -0,0 +1,17 @@
+# Python Program to find Perfect Number using For loop
+# A number is a perfect mumber if total of all positive divisors excluding\
+# the number itself is equal to that number. eg 6 has divisors 1,2,3,and 6\
+# so 1+2+3 = 6
+
+
+number = int(input("Please Enter any Number: "))
+total = 0
+for i in range(1, number):
+    if(number % i == 0):
+        total = total + i
+if (total == number):
+    print(" %d is a Perfect Number" %number)
+else:
+    print(" %d is not a Perfect Number" %number)
+
+

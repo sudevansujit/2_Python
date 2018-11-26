097da6a355fc663606214d1a4211dd13b17d0b8c
Sujit Sudevan <44827942+sudevansujit@users.noreply.github.com>
2018-11-26 18:47:06 +0530

    * * *

--- A1051_Char_in_Alphabets.py
+++ A1051_Char_in_Alphabets.py
@@ -5,3 +5,47 @@ if((ch >= 'a' and ch <= 'z') or (ch >= 'A' and ch <= 'Z')):
     print("The Given Character ", ch, "is an Alphabet")
 else:
     print("The Given Character ", ch, "is Not an Alphabet")
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+#        Please Enter a Character : d
+#        The Given Character  d is an Alphabet

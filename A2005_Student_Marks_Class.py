ae036fd398f806223ce8ff68a59521200b224fd4
Sujit Sudevan <44827942+sudevansujit@users.noreply.github.com>
2018-11-26 18:53:46 +0530

    * *

---
+++ A2005_Student_Marks_Class.py
@@ -0,0 +1,14 @@
+#Output Student Class
+
+score = int(input("Enter Student Score <= 100 : "))
+
+if (score >=35) and (score < 50):
+    print("You got IIIrd Class")
+elif (score >=50) and (score < 60):
+    print("You got IIrd Class")
+elif (score >=60) and (score <85):
+    print("You got Ist Class")
+elif (score >=85) and (score <= 100):
+    print("You got Distinction")
+else:
+    print("You Failed, Study Hard")

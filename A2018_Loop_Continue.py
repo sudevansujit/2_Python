a6dce7cf6b2dc75a1e7449d98d1bf48448aaa02c
Sujit Sudevan <44827942+sudevansujit@users.noreply.github.com>
2018-11-26 19:36:53 +0530

    * *

--- A2018_Loop_Continue.py
+++ A2018_Loop_Continue.py
@@ -4,3 +4,57 @@ for x in L:
     if x == 20:
         continue
     print(x)
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+#    Hello
+#    5
+#    Hello
+#    10
+#    Hello
+#    15
+#    Hello
+#    Hello
+#    25
+#    Hello
+#    30

139808dad8a3f6186648688c3d3d237af108f24d
Sujit Sudevan <44827942+sudevansujit@users.noreply.github.com>
2018-11-26 19:19:59 +0530

    * * *

--- A2016_Loop_Name_While_For.py
+++ A2016_Loop_Name_While_For.py
@@ -9,3 +9,54 @@ while (count<3):
     else:
         count = count +1
         print("\n")
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#    Sam
+#    Tan
+#    Jai
+
+
+#    Sam
+#    Tan
+#    Jai
+
+
+#    Sam
+#    Tan
+#    Jai

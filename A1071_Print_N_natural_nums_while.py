464ce1fe0780af6c8331ca65a2625c5be34f28d8
Sujit Sudevan <44827942+sudevansujit@users.noreply.github.com>
2018-11-26 12:13:08 +0530

    * *

--- A1071_Print_N_natural_nums_while.py
+++ A1071_Print_N_natural_nums_while.py
@@ -10,3 +10,43 @@ while ( i <= number):
     print (i, end = '  ')
     i = i + 1
 
+
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+#  Please Enter any Number: 10
+#  The List of Natural Numbers from 1 to 10 are
+#  1  2  3  4  5  6  7  8  9  10  

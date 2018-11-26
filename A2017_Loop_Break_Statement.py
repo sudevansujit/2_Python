fffacbdf701212d3461d4d1f95536a300d31f0a9
Sujit Sudevan <44827942+sudevansujit@users.noreply.github.com>
2018-11-26 19:33:35 +0530

    * *

---
+++ A2017_Loop_Break_Statement.py
@@ -0,0 +1,9 @@
+# Loop Statement
+
+L= [5,10,15,20,25,30]
+for x in L:
+    if x == 20:
+        print("Found 20")
+        break
+    else:
+        print(x)
---
+++ A2018_Loop_Continue.py
@@ -0,0 +1,6 @@
+L= [5,10,15,20,25,30]
+for x in L:
+    print("Hello")
+    if x == 20:
+        continue
+    print(x)

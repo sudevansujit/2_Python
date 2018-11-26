2857294760aff38e10047bc3152af0277eb112e4
Sujit Sudevan <44827942+sudevansujit@users.noreply.github.com>
2018-11-26 19:16:49 +0530

    * * *

---
+++ A2016_Loop_Name_While_For.py
@@ -0,0 +1,11 @@
+#Loops
+
+L = ["Sam", "Tan", "Jai"]
+count = 0
+while (count<3):
+    for item in L:
+        if count <3:
+            print(item)
+    else:
+        count = count +1
+        print("\n")

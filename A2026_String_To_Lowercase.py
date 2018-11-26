11eceb3f1ccd091ee5300e8c6298c2c645d3d4fe
Sujit Sudevan <44827942+sudevansujit@users.noreply.github.com>
2018-11-26 20:09:56 +0530

    * *

--- A2026_String_To_Lowercase.py
+++ A2026_String_To_Lowercase.py
@@ -6,3 +6,47 @@ string1 = string.lower()
  
 print("\nOriginal String in Uppercase  =  ", string)
 print("The Given String in Lowercase =  ", string1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#    O/P
+#    Original String in Uppercase  =   PYThon
+#    The Given String in Lowercase =   python

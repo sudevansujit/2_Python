2868c18f8520b80ea94cf22abb9d3f36385011fb
Sujit Sudevan <44827942+sudevansujit@users.noreply.github.com>
2018-11-26 19:44:46 +0530

    * * *

--- A2020_String_Charc_ASCII.py
+++ A2020_String_Charc_ASCII.py
@@ -4,3 +4,54 @@ str1 = input("Please Enter your Own String : ")
  
 for i in range(len(str1)):
     print("The ASCII Value of Character %c = %d" %(str1[i], ord(str1[i])))
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+#     O/P
+#     Please Enter your Own String : Python
+#     The ASCII Value of Character P = 80
+#     The ASCII Value of Character y = 121
+#     The ASCII Value of Character t = 116
+#     The ASCII Value of Character h = 104
+#     The ASCII Value of Character o = 111
+#     The ASCII Value of Character n = 110

db68c1b0ac00fb794574603df98283969dfd2da4
Sujit Sudevan <44827942+sudevansujit@users.noreply.github.com>
2018-11-27 12:18:56 +0530

    * * *

--- A2076_Largest_and_Smallest_Its_Index_List2.py
+++ A2076_Largest_and_Smallest_Its_Index_List2.py
@@ -1,4 +1,4 @@
-# Largest element and its index
+# Largest and amallest element and its index
 
 L1 = [ 99,23,34,5,67,65,90,89,100]
 
@@ -20,3 +20,36 @@ print(large_pos, "is the index position of the largest element""\n")
 
 print(smallest, "is the smallest element in the list")
 print(small_pos, "is the index position of the smallest element")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#    O/P
+#    100 is the largest element in the list
+#    8 is the index position of the largest element
+
+#    5 is the smallest element in the list
+#    3 is the index position of the smallest element

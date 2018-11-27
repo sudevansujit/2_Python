c41d59e489c3c977759394e047cc84d19f576869
Sujit Sudevan <44827942+sudevansujit@users.noreply.github.com>
2018-11-27 12:17:32 +0530

    * * *

---
+++ A2076_Largest_and_Smallest_Its_Index_List2.py
@@ -0,0 +1,22 @@
+# Largest element and its index
+
+L1 = [ 99,23,34,5,67,65,90,89,100]
+
+L2 = len(L1) 
+
+largest = 0
+smallest = 101
+
+for j in range(L2):
+    if L1[j] > largest:
+        largest = L1[j]
+        large_pos = j
+    if L1[j] < smallest:
+        smallest = L1[j]
+        small_pos = j
+        
+print(largest, "is the largest element in the list")
+print(large_pos, "is the index position of the largest element""\n")
+
+print(smallest, "is the smallest element in the list")
+print(small_pos, "is the index position of the smallest element")

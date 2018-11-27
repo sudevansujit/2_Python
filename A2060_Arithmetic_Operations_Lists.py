2088ee2889de6deca074ff6a8fffd16b7536a7c7
Sujit Sudevan <44827942+sudevansujit@users.noreply.github.com>
2018-11-27 11:13:42 +0530

    * *

--- A2060_Arithmetic_Operations_Lists.py
+++ A2060_Arithmetic_Operations_Lists.py
@@ -23,3 +23,34 @@ print("The List Items after Multiplication =  ", multi)
 print("The List Items after Division =  ", div)
 print("The List Items after Modulus =  ", mod)
 print("The List Items after Exponent =  ", expo)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#    O/P
+#    The List Items after Addition =   [15, 30, 45]
+#    The List Items after Subtraction =   [5, 10, 15]
+#    The List Items after Multiplication =   [50, 200, 450]
+#    The List Items after Division =   [2.0, 2.0, 2.0]
+#    The List Items after Modulus =   [0, 0, 0]
+#    The List Items after Exponent =   [100000, 10240000000000, 14348907000000000000000]

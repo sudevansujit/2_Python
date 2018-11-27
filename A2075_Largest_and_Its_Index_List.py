bea621ac54af096fea150dffdcf3e862334b15a5
Sujit Sudevan <44827942+sudevansujit@users.noreply.github.com>
2018-11-27 11:55:44 +0530

    * * *

---
+++ A2075_Largest_and_Its_Index_List.py
@@ -0,0 +1,15 @@
+# Largest element and its index
+
+L1 = [ 99,23,34,5,67,65,90,89,100]
+
+L2 = len(L1) 
+
+largest = 0
+
+for j in range(L2):
+    if L1[j] > largest:
+        largest = L1[j]
+        position = j
+
+print(largest, "is the largest element in the list")
+print(position, "is the index position of the element")
